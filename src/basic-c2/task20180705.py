e437c7f82f6a7687c27060be9ddcdaf7f4061348
n18021 <n18021@std.it-college.ac.jp>
2018-07-06 09:10:06 +0900

    7/5 課題

---
+++ src/basic-c2/task20180705.py
@@ -0,0 +1,19 @@
+#値段
+sake_n = 200
+tumami_n = 100
+yakitori_n = 100
+
+#個数
+sake_k = 2
+tumami_k = 1
+yakitori_k = 2
+
+#割引率
+rate = 0.8
+point = 150
+
+#計算
+kaikei = (sake_n * sake_k) + (tumami_n) + ((yakitori_n * yakitori_k) * rate) - (point)
+
+#結果
+print("買い物の合計は", kaikei,"円です")
