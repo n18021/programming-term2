f128fa1db82f65a021900d12636f6382c7299588
n18021 <n18021@std.it-college.ac.jp>
2018-07-19 13:29:49 +0900

    課題途中

---
+++ src/basic-c3/work/task20180718.py
@@ -0,0 +1,13 @@
+# スタバっぽいこと
+main_mane = {"DripCoffee": 280, "ColdBrewCoffee": 320, "CafeLate": 330, "SoyLatte": 380, "Cappuccino": 330, "CaramelFrappuccino": 470, "MacchaCreamFrappuccino": 470,}
+print(main_mane)
+
+option = {"LowFatMilk": 0, "NonFatMilk": 0, "SoyMilk": 50,
+          "DeepCoffee": 60, "WhipCream": 70, "CaramelShurup": 70,
+          "ChocoSource": 0, "DeCafe": 50}
+
+main_a = []
+optyon_a = []
+x = input("メインメニューを入力してください")
+for main_a in main_mane:
+    print()
